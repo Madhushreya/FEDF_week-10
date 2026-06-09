99b154b6710cb572e75c6e65ca909d4ce3c6d18e
Madhu Shreya <madhushreya0829@gmail.com>
2026-06-09 20:19:36 +0530

    Create Dashboard.jsx

---
+++ Dashboard.jsx
@@ -0,0 +1,18 @@
+
+
+
+import { Link } from "react-router-dom";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Librarian Dashboard</h2>
+
+      <Link to="/deletebook">
+        Delete Books
+      </Link>
+    </div>
+  );
+}
+
+export default Dashboard;
